7b319f7656afe030050d1c477a542afa51be8457
VendelaAJ <vendela.swenson@hotmail.se>
2021-11-30 18:31:02 +0100

    added mediaquery

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ hamburgerBar.addEventListener("click", () => {
     navBar.classList.add("barActive");
     for (let i = 0; i < sections.length; i++)
       sections[i].style.display = "initial";
+    logo.style.display = "initial";
   } else {
     active = false;
     navBar.classList.remove("barActive");
     for (let i = 0; i < sections.length; i++)
       sections[i].style.display = "none";
+    logo.style.display = "none";
   }
 });
