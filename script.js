6824e5b069fcdf97ac015eb3b7b02e988e91fe60
VendelaAJ <vendela.swenson@hotmail.se>
2021-12-07 12:49:18 +0100

    added responses

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ let active = false;
 hamburgerBar.addEventListener("click", () => {
   if (!active) {
     active = true;
+    navBar.style.display = "flex";
     navBar.classList.add("barActive");
+    hamburgerBar.classList.add("activeBtn");
     for (let i = 0; i < sections.length; i++)
       sections[i].style.display = "initial";
     logo.style.display = "initial";
   } else {
     active = false;
     navBar.classList.remove("barActive");
+    hamburgerBar.classList.remove("activeBtn");
+    navBar.style.display = "none";
     for (let i = 0; i < sections.length; i++)
       sections[i].style.display = "none";
     logo.style.display = "none";
